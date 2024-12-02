7aca092abedfe488c8221a50e6fad353139a2958
Jlexender <aleksandrdmitryevich@gmail.com>
2024-12-02 23:52:56 +0300

    finished

--- internal/handlers/area.go
+++ internal/handlers/area.go
@@ -10,11 +10,11 @@ import (
 )
 
 type AreaRequest struct {
-	X              int `json:"x"`
-	Y              int `json:"y"`
-	Radius         int `json:"radius"`
-	UnitMultiplier int `json:"unit_multiplier"`
-	CanvasLimit    int `json:"canvas_limit"`
+	X              int     `json:"x"`
+	Y              int     `json:"y"`
+	Radius         float64 `json:"radius"`
+	UnitMultiplier int     `json:"unit_multiplier"`
+	CanvasLimit    int     `json:"canvas_limit"`
 }
 
 func CheckIfInArea(c *gin.Context) {
@@ -24,29 +24,28 @@ func CheckIfInArea(c *gin.Context) {
 		return
 	}
 
-	x := request.X
-	y := request.Y
-	unit := request.UnitMultiplier * request.Radius
-	lim := request.CanvasLimit
-
-	username, _ := c.Get("username")
-
+	unit := float64(request.UnitMultiplier) * request.Radius
+	limit := float64(request.CanvasLimit)
 	var result bool
-
-	if x > lim && y < lim {
-		result = 2*(x-lim) <= (lim-y)+unit
-	} else if x <= lim && y >= lim {
-		result = 2*(y-lim) <= (x-lim)+unit
-	} else if x < lim && y < lim {
-		result = x > lim-unit && y > lim-unit
+	x, y := float64(request.X), float64(request.Y)
+	if x > limit && y < limit {
+		result = false
+	} else if x <= limit && y >= limit {
+		result = 2*(y-400) <= (x-400)+unit
+	} else if x < limit && y < limit {
+		result = x > 400-unit && y > 400-unit
 	} else {
-		result = (lim-x)*(lim-x)+(lim-y)*(lim-y) <= unit*unit/4
+		result = (400-x)*(400-x)+(400-y)*(400-y) < (unit/2)*(unit/2)
 	}
 
+	realX := (x - limit) / float64(request.UnitMultiplier)
+	realY := (limit - y) / float64(request.UnitMultiplier)
+	username, _ := c.Get("username")
+
 	areaCheck := models.AreaCheck{
-		X:        x,
-		Y:        y,
-		Radius:   unit,
+		X:        realX,
+		Y:        realY,
+		Radius:   request.Radius,
 		Result:   result,
 		SendTime: time.Now().Format("2006-01-02 15:04:05"),
 		Author:   username.(string),
--- internal/models/area.go
+++ internal/models/area.go
@@ -1,10 +1,10 @@
 package models
 
 type AreaCheck struct {
-	X        int    `json:"x"`
-	Y        int    `json:"y"`
-	Radius   int    `json:"radius"`
-	Result   bool   `json:"result"`
-	SendTime string `json:"send_time"`
-	Author   string `json:"author"`
+	X        float64 `json:"x"`
+	Y        float64 `json:"y"`
+	Radius   float64     `json:"radius"`
+	Result   bool    `json:"result"`
+	SendTime string  `json:"send_time"`
+	Author   string  `json:"author"`
 }
